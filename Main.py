11e812f9db4facab7ca5815a7c76c16bcedbdea7
Aurora <Programming.aurora@gmail.com>
2020-04-25 17:33:39 +0100

    Created first file

---
+++ Main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+import Secret
+
+description = "desc"
+
+bot = commands.Bot(command_prefix='?', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+bot.run(Secret.token)
