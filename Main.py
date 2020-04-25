99e0e596e3a9b6c696c8450b391f4d8b3197ed63
Aurora <Programming.aurora@gmail.com>
2020-04-25 23:44:00 +0100

    Changed prefix to a!

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 import discord
 from discord.ext import commands
 
+from DemocracyCog import DemocracyCog
 import Secret
 
 description = "desc"
 
-bot = commands.Bot(command_prefix='?', description=description)
+bot = commands.Bot(command_prefix='a!', description=description)
 
 @bot.event
 async def on_ready():
@@ -13,5 +14,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
+bot.add_cog(DemocracyCog(bot))
 
 bot.run(Secret.token)
