f04fcd933fac295d6e45469d6ec14bfce027ed76
Aurora <Programming.aurora@gmail.com>
2020-05-01 02:54:20 +0100

    Added ability to create votes

--- DemocracyCog.py
+++ DemocracyCog.py
@@ -1,8 +1,12 @@
+import asyncio
+
 import discord
 from discord.ext import commands
 from discord.ext.commands import Cog
 
 import DatabaseManager as dm
+
+user_not_registered = "You do not have an account!"
 
 class DemocracyCog(Cog):
     
@@ -19,7 +23,29 @@ class DemocracyCog(Cog):
         id = ctx.author.id
         try:
             dm.create_user(id)
-        except dm.RowExistsError as e:
+        except dm.RowExistsError:
             await ctx.send("You already have an account!")
         else:
             await ctx.send("Successfully registered!")
+
+    @commands.command()
+    async def createvote(self, ctx, votename):
+        if dm.vote_exists(votename):
+            await ctx.send("Vote already exists!")
+            return
+        
+        def check(m):
+            return m.author.id == ctx.author.id and m.channel == ctx.channel
+
+        await ctx.send("Send line separated choices")
+        options: str
+        try:
+            options = await self.bot.wait_for("message", check=check, timeout=90)  
+        except asyncio.TimeoutError:
+            ctx.send("Timed out")
+            return
+        options = options.content.split("\n")
+        
+        dm.create_vote(votename, *options)
+        await ctx.send("Vote created successfully")
+        
